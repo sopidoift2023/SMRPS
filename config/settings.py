98dda8826ca1ab467a5df7ccf4ad0c7f7cbd97a7
Oriere Emmanuel <orieremmanuel@gmail.com>
2026-03-14 14:55:14 +0100

    Migrate database to Supabase

--- config/settings.py
+++ config/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        default=os.environ.get('DATABASE_URL', f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
         conn_max_age=600,
         conn_health_checks=True,
     )
